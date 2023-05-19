f7f92382094c23b9abd79ec1334f7ee8cb3c6bf8
JoseFrancisc0 <jose.wong@utec.edu.pe>
2023-05-19 13:59:26 -0500

    Initialized C++ Project

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/josew/Desktop/utec/5to Ciclo - 2023.1/Algoritmos y Estructuras de Datos/AED_Grupo3_Proyecto
+# Build directory: C:/Users/josew/Desktop/utec/5to Ciclo - 2023.1/Algoritmos y Estructuras de Datos/AED_Grupo3_Proyecto/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
