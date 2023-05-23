78b4303e1ba281312a92cc1d540f5d58d668d8f3
JoseFrancisc0 <jose.wong@utec.edu.pe>
2023-05-23 11:13:43 -0500

    Block implemented with SSL hash functions (works at least in CLion, VSCode not tested)

--- block.h
+++ block.h
@@ -2,8 +2,8 @@
 #define BLOCK_H
 
 #include "transaction.h"
-#include <vector>
 #include <string>
+#include <openssl/sha.h>
 using namespace std;
 
 class Block{
@@ -16,8 +16,16 @@ class Block{
             blockhash = calculateHash();
         }
 
-        string calculateHash(){
+        string calculateHash() const{
+            string data = transaction.getSender() + transaction.getRecipient() + to_string(transaction.getAmount()) + prevHash;
+            unsigned char hash[SHA256_DIGEST_LENGTH];
+            SHA256_CTX sha256;
+            SHA256_Init(&sha256);
+            SHA256_Update(&sha256, data.c_str(), data.length());
+            SHA256_Final(hash, &sha256);
 
+            string hashString(reinterpret_cast<const char*>(hash), SHA256_DIGEST_LENGTH);
+            return hashString;
         }
 
         ~Block() = default;
