2e0e2c6ceda411f61b5cfc7fb4d4683fcd6bd979
JoseFrancisc0 <jose.wong@utec.edu.pe>
2023-05-24 14:29:27 -0500

    Basic structure of blockchain

--- blockchain.h
+++ blockchain.h
@@ -5,6 +5,27 @@
 #include <vector>
 using namespace std;
 
-class BlockChain{};
+class BlockChain{
+    private:
+        struct Node{
+            Block block;
+            Node* next = nullptr;
+
+            Node(Block _block) : block(_block){};
+        };
+        Node* head = nullptr;
+
+    public:
+        BlockChain() = default;
+
+        ~BlockChain(){
+            Node* current = head;
+            while(current != nullptr){
+                Node* temp = current;
+                current = current->next;
+                delete temp;
+            }
+        }
+};
 
 #endif //BLOCKCHAIN_H
