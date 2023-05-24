9d51391ff4239306cd74dfe3c49e521b522e221d
JoseFrancisc0 <jose.wong@utec.edu.pe>
2023-05-24 14:49:32 -0500

    Blockchain functions

--- blockchain.h
+++ blockchain.h
@@ -13,6 +13,7 @@ class BlockChain{
 
             Node(Block _block) : block(_block){};
         };
+
         Node* head = nullptr;
 
     public:
@@ -26,6 +27,17 @@ class BlockChain{
                 delete temp;
             }
         }
+
+        void addTransaction(Transaction _transaction); /// Inserta transacciones
+        int getTransactionCount(); /// El nro de transacciones (y de bloques)
+        Block getLatestBlock(); /// El bloque mas reciente
+        Block getBlockByIndex(); /// Busca por indice
+        Block search(int key); /// Equal to X
+        vector<Block> range_search(int begin, int end); /// Between X and Y
+        void cascadeRecalculation(); /// Recalculo en cascada
+        Block deleteRecordatIndex(int key); /// Elimina registros por indice
+        bool validateBlockchain(); /// Validacion de la blockchain
+        void loadFromTextfile(); /// Carga de datos por csv
 };
 
 #endif //BLOCKCHAIN_H
