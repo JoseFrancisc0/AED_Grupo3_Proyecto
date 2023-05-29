cd3ffab8cceede4b8cba37762e0f664eef2e0c5b
JoseFrancisc0 <jose.wong@utec.edu.pe>
2023-05-29 17:44:32 -0500

    avl things

--- avl.h
+++ avl.h
@@ -1,6 +1,9 @@
 #ifndef AED_PROYECTO_AVL_H
 #define AED_PROYECTO_AVL_H
 
+#include <vector>
+using namespace std;
+
 template<typename TK, typename TV>
 class AVL{
     private:
@@ -24,11 +27,28 @@ class AVL{
         };
         Node<TK,TV>* root = nullptr;
 
+        void insert(Node<TK,TV>* _node, TK _key, TV _value);
+        void remove(Node<TK,TV>* _node, TK _key);
+        void range_search(Node<TK,TV>* _node, TK _begin, TK _end, vector<TV>& v);
+
+
     public:
         AVL() = default;
-        void insert();
-        TV remove();
-        TV search();
+
+        void insert(TK _key, TV _value){
+            insert(this->root, _key, _value);
+        }
+
+        void remove(TK _key){
+            remove(this->root, _key);
+        }
+
+        vector<TV> range_search(TK begin, TK end){
+            vector<TV> result;
+            range_search(root, begin, end, result);
+            return result;
+        }
+
         ~AVL() = default;
 };
 
