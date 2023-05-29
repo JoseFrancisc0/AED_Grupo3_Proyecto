6401480a96eecd4b6471c56a061a0530f679fe19
JoseFrancisc0 <jose.wong@utec.edu.pe>
2023-05-29 16:57:59 -0500

    Node struct implementation

--- avl.h
+++ avl.h
@@ -1,25 +1,34 @@
 #ifndef AED_PROYECTO_AVL_H
 #define AED_PROYECTO_AVL_H
 
-template<typename T>
-struct Node{
-    T data;
-    int height = 0;
-    Node* left = nullptr;
-    Node* right = nullptr;
-
-    Node(T _data) : data(_data){};
-};
-
-template<typename T>
+template<typename TK, typename TV>
 class AVL{
     private:
-        Node<T> root;
+        template<typename UK, typename UV>
+        struct Node{
+            UK key;
+            UV value;
+            int height = 0;
+            Node* left = nullptr;
+            Node* right = nullptr;
+
+            Node(UK _key, UV _value): key(_key), value(_value){};
+
+            void killself(){
+                if(left != nullptr)
+                    left->killself();
+                if(right != nullptr)
+                    right->killself();
+                delete this;
+            }
+        };
+        Node<TK,TV>* root = nullptr;
+
     public:
         AVL() = default;
         void insert();
-        T remove();
-        T search();
+        TV remove();
+        TV search();
         ~AVL() = default;
 };
 
