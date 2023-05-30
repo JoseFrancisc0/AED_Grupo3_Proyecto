b845c109a33ae18ec668bb74b65eb03762be5ecb
Franco Bolaños <54276760+FrancoBolanos@users.noreply.github.com>
2023-05-29 19:19:17 -0500

    Update avl.h
    Added:
    -Insert
    
    Modified:
    -Position of node structure

--- avl.h
+++ avl.h
@@ -3,53 +3,75 @@
 
 #include <vector>
 using namespace std;
+template<typename UK, typename UV>
+struct Node{
+    UK key;
+    UV value;
+    int height = 0;
+    Node* left = nullptr;
+    Node* right = nullptr;
+
+    Node(UK _key, UV _value): key(_key), value(_value){};
+
+    void killself(){
+        if(left != nullptr)
+            left->killself();
+        if(right != nullptr)
+            right->killself();
+        delete this;
+    }
+};
 
 template<typename TK, typename TV>
 class AVL{
-    private:
-        template<typename UK, typename UV>
-        struct Node{
-            UK key;
-            UV value;
-            int height = 0;
-            Node* left = nullptr;
-            Node* right = nullptr;
-
-            Node(UK _key, UV _value): key(_key), value(_value){};
-
-            void killself(){
-                if(left != nullptr)
-                    left->killself();
-                if(right != nullptr)
-                    right->killself();
-                delete this;
-            }
-        };
-        Node<TK,TV>* root = nullptr;
-
-        void insert(Node<TK,TV>* _node, TK _key, TV _value);
-        void remove(Node<TK,TV>* _node, TK _key);
-        void range_search(Node<TK,TV>* _node, TK _begin, TK _end, vector<TV>& v);
-
+    private:  
+        Node<TK,TV>* root;;
+        void insert(Node<TK,TV> *&node, TK key, TV value);
+        void remove(Node<TK,TV> *&node, TK key);
+        void range_search(Node<TK,TV>* node, TK begin, TK end, vector<TV>& v);
+        void search(Node<TK,TV>* node, TK key);
+        void balance(Node<TK,TV> *&node);
 
     public:
-        AVL() = default;
+        AVLTree() : root(nullptr) {}
 
-        void insert(TK _key, TV _value){
-            insert(this->root, _key, _value);
+        void insert(TK key, TV value){
+            insert(this->root, key, value);
         }
 
-        void remove(TK _key){
-            remove(this->root, _key);
+        void remove(TK key){
+            remove(this->root, key);
+        }
+
+        TV search(TK _key){
+            return search(this->root, key);
         }
 
         vector<TV> range_search(TK begin, TK end){
             vector<TV> result;
-            range_search(root, begin, end, result);
+            range_search(this->root, begin, end, result);
             return result;
         }
 
-        ~AVL() = default;
+        ~AVL(){
+            if(this->root != nullptr)
+                this->root->killSelf();
+        }
 };
 
-#endif //AED_PROYECTO_AVL_H
+template<typename TK, typename TV>
+void insert(Node<TK,TV> *&node, TK key, TV value){
+    if (node == nullptr)
+        node = new Node<TK,TV>(key,value);
+    else if (key < node->key)
+        insert(node->left, key, value);
+    else if (key > node->key)
+        insert(node->right, key, value);
+    
+    balance(node);
+}
+
+template<typename TK, typename TV>
+void balance(Node<TK,TV> *&node){
+    
+}
