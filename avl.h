8a013192033ceb8e5b2737ac69593c0e687a75de
JoseFrancisc0 <jose.wong@utec.edu.pe>
2023-05-29 22:34:11 -0500

    Implemented private recursive range_search() method

--- avl.h
+++ avl.h
@@ -29,6 +29,61 @@ class AVL{
     private:  
         Node<TK,TV>* root;
 
+        int height(Node<TK,TV> *node){
+            if (node == nullptr)
+                return -1;
+            else
+                return node->height;
+        }
+
+        void updateHeight(Node<TK,TV> *node){
+            node->height = max(height(node->left), height(node->right)) +1;
+        }
+
+        void left_rota(Node<TK,TV> *&node){
+            Node<TK,TV> *k1 = node->left;
+            node->left = k1->right;
+            k1->right = node;
+            node->height = max(height(node->left), height(node->right)) + 1;
+            k1->height = max(height(k1->left), node->height) + 1;
+            node = k1;
+        }
+
+        void right_rota(Node<TK,TV> *&node){
+            Node<TK,TV> *k1 = node->right;
+            node->right = k1->left;
+            k1->left = node;
+            node->height = max(height(node->right), height(node->left)) + 1;
+            k1->height = max(height(k1->right), node->height) + 1;
+            node = k1;
+        }
+
+        void balance(Node<TK,TV> *&node){
+            if(node==nullptr)
+                throw std::out_of_range("AVL empty");
+
+            if(height(node->left)-height(node->right)>1){
+                if(height(node->left->left) >= height(node->left->right))
+                    left_rota(node);
+                else{
+                    right_rota(node->left);
+                    left_rota(node);
+                }
+            }
+            else{
+                if(height(node->left)-height(node->right)<-1){
+                    if(height(node->right->right) >= height(node->right->left))
+                        right_rota(node);
+                    else{
+                        left_rota(node->right);
+                        right_rota(node);
+                    }
+                }
+            }
+
+            updateHeight(node);
+        }
+
         void insert(Node<TK,TV> *&node, TK key, TV value){
             if (node == nullptr)
                 node = new Node<TK,TV>(key,value);
@@ -66,61 +121,17 @@ class AVL{
             balance(node);
         }
 
-        void range_search(Node<TK,TV>* node, TK begin, TK end, vector<TV>& v);
-        
-        void balance(Node<TK,TV> *&node){
-            if(node==nullptr)
+        void range_search(Node<TK,TV>* node, TK begin, TK end, vector<TV>& v){
+            if(node == nullptr)
                 throw std::out_of_range("AVL empty");
 
-            if(height(node->left)-height(node->right)>1){
-                if(height(node->left->left) >= height(node->left->right))
-                    left_rota(node);
-                else{
-                    right_rota(node->left);
-                    left_rota(node);
-                }
-            }
-            else{
-                if(height(node->left)-height(node->right)<-1){
-                    if(height(node->right->right) >= height(node->right->left))
-                        right_rota(node);
-                    else{
-                        left_rota(node->right);
-                        right_rota(node);
-                    }
-                }
-            }
+            if(node->key >= begin && node->key <= end)
+                v.push_back(node->value);
 
-            updateHeight(node);
-        }
-
-        int height(Node<TK,TV> *node){
-            if (node == nullptr)
-                return -1;
-            else
-                return node->height;
-        }
-
-        void left_rota(Node<TK,TV> *&node){
-            Node<TK,TV> *k1 = node->left;
-            node->left = k1->right;
-            k1->right = node;
-            node->height = max(height(node->left), height(node->right)) + 1;
-            k1->height = max(height(k1->left), node->height) + 1;
-            node = k1;
-        }
-
-        void right_rota(Node<TK,TV> *&node){
-            Node<TK,TV> *k1 = node->right;
-            node->right = k1->left;
-            k1->left = node;
-            node->height = max(height(node->right), height(node->left)) + 1;
-            k1->height = max(height(k1->right), node->height) + 1;
-            node = k1;
-        }
-
-        void updateHeight(Node<TK,TV> *node){
-            node->height = max(height(node->left), height(node->right)) +1;
+            if(node->key > begin)
+                range_search(node->left, begin, end, v);
+            if(node->key < end)
+                range_search(node->right, begin, end, v);
         }
 
     public:
