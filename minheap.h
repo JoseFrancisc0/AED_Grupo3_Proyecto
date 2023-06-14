8b6316f8e904bfa604eeedcf381262b39306d49d
JoseFrancisc0 <jose.wong@utec.edu.pe>
2023-06-14 18:15:33 -0500

    Implemented minheap methods

--- minheap.h
+++ minheap.h
@@ -1,6 +1,7 @@
 #ifndef AED_PROYECTO_MINHEAP_H
 #define AED_PROYECTO_MINHEAP_H
 
+#include <stdexcept>
 #include <vector>
 using namespace std;
 
@@ -9,13 +10,33 @@ class MinHeap{
     private:
         vector<pair<TK, TV>> heap;
 
-        int parent(int i);
-        void heapifyUp(int i);
+        int parent(int i){
+            return (i - 1)/2;
+        }
+
+        void heapifyUp(int i){
+            while(i > 0 && heap[parent(i)].first > heap[i].first){
+                swap(heap[i], heap[parent(i)]);
+                i = parent(i);
+            }
+        }
 
     public:
         MinHeap() = default;
-        void insert(TK _key, TV _value);
-        TV getMin();
+
+        void insert(TK _key, TV _value){
+            heap.emplace_back(_key, _value);
+            int index = heap.size() - 1;
+            heapifyUp(index);
+        }
+
+        TV getMin(){
+            if(heap.empty())
+                throw std::out_of_range("MinHeap Empty");
+
+            return heap[0];
+        }
+
         ~MinHeap() = default;
 };
 
