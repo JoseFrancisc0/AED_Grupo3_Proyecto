e5a319e5f3e849760eecc21d8dbfd45916636ea4
JoseFrancisc0 <jose.wong@utec.edu.pe>
2023-06-14 17:52:57 -0500

    Created MinHeap class skeleton

---
+++ minheap.h
@@ -0,0 +1,11 @@
+#ifndef AED_PROYECTO_MINHEAP_H
+#define AED_PROYECTO_MINHEAP_H
+
+template<typename TK, typename TV>
+class MinHeap{
+    public:
+        MinHeap() = default;
+        ~MinHeap() = default;
+};
+
+#endif //AED_PROYECTO_MINHEAP_H
