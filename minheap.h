afd0d154272a5dc09d9f266ec0d6494731198d62
JoseFrancisc0 <jose.wong@utec.edu.pe>
2023-06-14 18:07:26 -0500

    Declared functions needed for minHeap

--- minheap.h
+++ minheap.h
@@ -1,10 +1,21 @@
 #ifndef AED_PROYECTO_MINHEAP_H
 #define AED_PROYECTO_MINHEAP_H
 
+#include <vector>
+using namespace std;
+
 template<typename TK, typename TV>
 class MinHeap{
+    private:
+        vector<pair<TK, TV>> heap;
+
+        int parent(int i);
+        void heapifyUp(int i);
+
     public:
         MinHeap() = default;
+        void insert(TK _key, TV _value);
+        TV getMin();
         ~MinHeap() = default;
 };
 
