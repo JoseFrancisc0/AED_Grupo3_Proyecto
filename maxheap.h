5cad5d1ed77673a3c52a4344211b44bd8329d82a
JoseFrancisc0 <jose.wong@utec.edu.pe>
2023-06-15 10:45:29 -0500

    Added MaxHeap class skeleton

---
+++ maxheap.h
@@ -0,0 +1,11 @@
+#ifndef AED_PROYECTO_MAXHEAP_H
+#define AED_PROYECTO_MAXHEAP_H
+
+template<typename TK, typename TV>
+class MaxHeap{
+public:
+    MaxHeap() = default;
+    ~MaxHeap() = default;
+};
+
+#endif //AED_PROYECTO_MAXHEAP_H
