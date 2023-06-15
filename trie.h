76ff904f0ef2998b2f36ad9289544a76e666525d
JoseFrancisc0 <jose.wong@utec.edu.pe>
2023-06-15 17:47:23 -0500

    Declared public functions for trie

--- trie.h
+++ trie.h
@@ -1,9 +1,19 @@
 #ifndef AED_PROYECTO_TRIE_H
 #define AED_PROYECTO_TRIE_H
 
+#include <string>
+#include <vector>
+using namespace std;
+
 class Trie{
+private:
+    struct TrieNode{};
+    TrieNode* root;
 public:
     Trie() = default;
+    void insert(string _string);
+    vector<string> search(string _prefix);
+    void clear();
     ~Trie() = default;
 };
 
