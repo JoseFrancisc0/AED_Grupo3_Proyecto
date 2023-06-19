9d46e795246446e86e7ae5795a0783b58925d86d
JoseFrancisc0 <jose.wong@utec.edu.pe>
2023-06-19 16:56:20 -0500

    Added suffix tree class skeleton for the contains search criteria

---
+++ suffixtree.h
@@ -0,0 +1,16 @@
+#ifndef AED_PROYECTO_SUFFIXTREE_H
+#define AED_PROYECTO_SUFFIXTREE_H
+
+#include <stdexcept>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class SuffixTree{
+private:
+public:
+    SuffixTree() = default;
+    ~SuffixTree() = default;
+};
+
+#endif //AED_PROYECTO_SUFFIXTREE_H
