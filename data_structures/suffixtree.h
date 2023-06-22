e66910310bcaa4b9b57e54235276a315c91bda4a
JoseFrancisc0 <jose.wong@utec.edu.pe>
2023-06-22 14:40:37 -0500

    Format things

--- data_structures/suffixtree.h
+++ data_structures/suffixtree.h
@@ -6,56 +6,56 @@
 using namespace std;
 
 class SuffixTree{
-private:
-    struct STNode{
-        unordered_map<char, STNode*> children;
-        vector<string> words;
-    };
+    private:
+        struct STNode{
+            unordered_map<char, STNode*> children;
+            vector<string> words;
+        };
 
-    STNode* root;
+        STNode* root;
 
-    void clear(STNode* node){
-        for(auto child : node->children)
-            clear(child.second);
+        void clear(STNode* node){
+            for(auto child : node->children)
+                clear(child.second);
 
-        delete node;
-    }
+            delete node;
+        }
 
-public:
-    SuffixTree() : root(nullptr) {};
+    public:
+        SuffixTree() : root(nullptr) {};
 
-    void insert(string str){
-        for (size_t i = 0; i < str.length(); i++) {
-            STNode* current = root;
-            for (size_t j = i; j < str.length(); j++) {
-                if (current->children.find(str[j]) == current->children.end()) {
-                    current->children[str[j]] = new STNode();
+        void insert(string str){
+            for (size_t i = 0; i < str.length(); i++) {
+                STNode* current = root;
+                for (size_t j = i; j < str.length(); j++) {
+                    if (current->children.find(str[j]) == current->children.end()) {
+                        current->children[str[j]] = new STNode();
+                    }
+                    current = current->children[str[j]];
+                    current->words.push_back(str);
                 }
-                current = current->children[str[j]];
-                current->words.push_back(str);
             }
         }
-    }
 
-    vector<string> search(string pattern){
-        STNode* current = root;
-        for (char c : pattern) {
-            if (current->children.find(c) == current->children.end()) {
-                return {}; // pattern not found
+        vector<string> search(string pattern){
+            STNode* current = root;
+            for (char c : pattern) {
+                if (current->children.find(c) == current->children.end()) {
+                    return {}; // pattern not found
+                }
+                current = current->children[c];
             }
-            current = current->children[c];
+            return current->words;
         }
-        return current->words;
-    }
 
-    void clear(){
-        clear(root);
-        root = new STNode();
-    }
+        void clear(){
+            clear(root);
+            root = new STNode();
+        }
 
-    ~SuffixTree(){
-        clear(root);
-    };
+        ~SuffixTree(){
+            clear(root);
+        };
 };
 
 #endif //SUFFIXTREE_H
